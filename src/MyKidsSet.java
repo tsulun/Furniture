86296d07d7d8a156ebd3dd69b77d1c8ddb804bbf
SedaYurdakul <Seda.ztbk@gmail.com>
2021-07-04 20:30:24 +0100

    added some methods into MyKidsSet class

--- src/MyKidsSet.java
+++ src/MyKidsSet.java
@@ -22,7 +22,59 @@ public abstract class MyKidsSet {
         furName = newFurName;
         price = 0.0;
     }
+    public double getPrice() {
+        return this.price;
+    }
+    public String showPrice()
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("\nPlease enter the kid's set Id : ");
+        kidsId = input.nextLine();
+        System.out.print("Pease enter the kid's furniture name :");
+        furName = input.nextLine();
 
 
+        switch(kidsId)
+        {
+            case("IK001"):
+                price = 45.00;
+                System.out.print("Price is RM :" + price);
+                break;
+            case("IK002"):
+                price = 100.00;
+                System.out.print("Price is RM :" + price);
+                break;
+            case("IK003"):
+                price = 250.00;
+                System.out.print("Price is RM :" + price);
+                break;
+            case("IK004"):
+                price = 150.00;
+                System.out.print("Price is RM :" + price);
+                break;
+            case("IK005"):
+                price = 450.00;
+                System.out.print("Price is RM :" + price);
+                break;
+            default:
+                System.out.println("Invalid choice!!");
+                System.exit(0);
+        }
+        return kidsId;
+    }
+
+    public void mysteryGift()
+    {
+        JDialog.setDefaultLookAndFeelDecorated(true); //using JOptionPane
+        System.out.print("\n");
+        System.out.print("Congrat's. You're entitled to get a mystery gift from us!!!");
+        Object [] selectionValues = {"Little Tayo", "Barbie", "Thomas & Friends",
+                "Roborcar Poli"};
+        String initialSelection = "Key Chain";
+        Object selection = JOptionPane.showInputDialog(null, "Mystery Gift",
+                "Choose Your Gift", JOptionPane.QUESTION_MESSAGE, null,
+                selectionValues, initialSelection);
+        System.out.println("\nYou have choose : " + selection);
+    }
 
 }
