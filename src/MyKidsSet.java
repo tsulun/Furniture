bab396f467f6d636b6315074c424bfa3e1e8d920
SedaYurdakul <Seda.ztbk@gmail.com>
2021-07-04 20:24:37 +0100

    added variables and constructors

--- src/MyKidsSet.java
+++ src/MyKidsSet.java
@@ -1,2 +1,28 @@
-public class MyKidsSet {
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import javax.swing.JDialog;
+
+public abstract class MyKidsSet {
+
+    private String kidsId, furName;
+    private double price;
+    public MyKidsSet()
+    {
+        kidsId = "";
+        furName = "";
+        price = 0.0;
+    }
+
+    public MyKidsSet(double p) {
+        price = p;
+    }
+    public MyKidsSet(String newId, String newFurName, double newPrice)
+    {
+        kidsId = newId;
+        furName = newFurName;
+        price = 0.0;
+    }
+
+
+
 }
