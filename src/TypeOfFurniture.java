cc2de278b5f912bcc564215294bfc8b0969328bf
yahyaayabakan <ayabakanyahya@gmail.com>
2021-07-04 18:52:00 +0100

    Added second part methods

--- src/TypeOfFurniture.java
+++ src/TypeOfFurniture.java
@@ -32,4 +32,33 @@ public class TypeOfFurniture {
             System.out.println((i+1) + ". " + s.getName());
         }
     }
+
+    public TypeOfFurniture(int m,String n, String j){
+        name = n;
+        memberId = m;
+        furnitureList = new Purchaser[10];
+    }
+    public String getName(){
+        return name;
+    }
+    public int getMemberId(){
+        return memberId;
+    }
+    public void addFurniture(Purchaser p1){
+        furnitureList[numOfFurniture]=p1;
+        numOfFurniture++;
+    }
+    public int getNumOfFurniture(){
+        return numOfFurniture;
+    }
+    public void printAllInfo(){
+        System.out.println("\nThis furniture set was taken by : " + name);
+        System.out.println(name + " was taken " + numOfFurniture + " set (s).");
+        System.out.println("List of furniture(s) taken : ");
+
+        for(int i = 0;i<numOfFurniture; i++){
+            Purchaser s = (Purchaser)furnitureList[i];
+            System.out.println((i+1) + ". " + s.getName());
+        }
+    }
 }
