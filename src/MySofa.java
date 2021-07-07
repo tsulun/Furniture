cf20f85bd4f564caa5a662091388043381c6ea5e
tsulun <tsulun16@gmail.com>
2021-07-07 14:25:33 +0100

    added variables

--- src/MySofa.java
+++ src/MySofa.java
@@ -2,5 +2,10 @@ import java.util.Scanner;
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 
-public class MySofa {
+public class MySofa extends MyFurniture {
+    private String sofaId;
+    private String sofaName;
+    private double discount;
+    private double price;
+
 }
