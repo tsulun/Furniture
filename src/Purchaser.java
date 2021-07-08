88e8ff28eb8b9b2d371a5984db1c2e4ec0fe0a74
TeodoruMarin <teodoru652@gmail.com>
2021-07-08 13:39:18 +0100

    Codes added Purchaser

---
+++ src/Purchaser.java
@@ -0,0 +1,26 @@
+package src;
+
+    public class Purchaser {
+        private final int id;
+        private final String name;
+        private String furnitureType;
+
+        public Purchaser(int i, String s, String n) {
+            id = i;
+            name = n;
+            furnitureType = s;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getID() {
+            return id;
+        }
+
+        public String getFurnitureType() {
+            return furnitureType;
+        }
+    }
+
