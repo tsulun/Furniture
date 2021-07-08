d0c921abdeeaa577cef0fbd482538a2a31fc1bf0
emanuelasavi <asaviemanuel@gmail.com>
2021-07-08 12:32:15 +0100

    Member Class is ready for review

--- src/Member.java
+++ src/Member.java
@@ -1,2 +1,24 @@
 public class Member {
+
+    private String name, color;
+    private double price;
+
+    public Member(String name, String color, double price) {
+        this.name = name;
+        this.color = color;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
 }
